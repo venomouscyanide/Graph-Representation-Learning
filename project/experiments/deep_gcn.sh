c379b40750d93ef34a2b2abd048996c10fd68f8a
Paul Louis <iampaullouis@gmail.com>
2021-12-09 16:54:40 -0500

    Start with deep model exp scripts

---
+++ project/experiments/deep_gcn.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+echo "With norm and degree_information"
+python -m project.shallow_models.model_trainer --dataset "karate" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_norm_degree_information_karate" --use_norm --degree_information
+python -m project.shallow_models.model_trainer --dataset "cora" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_norm_degree_information_cora" --use_norm --degree_information
+python -m project.shallow_models.model_trainer --dataset "citeseer" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_norm_degree_information_citeceer" --use_norm --degree_information
+python -m project.shallow_models.model_trainer --dataset "pubmed" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_norm_degree_information_pubmed" --use_norm --degree_information
+python -m project.shallow_models.model_trainer --dataset "computers" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_norm_degree_information_computers" --use_norm --degree_information
+python -m project.shallow_models.model_trainer --dataset "photo" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_norm_degree_information_photo" --use_norm --degree_information
+echo "With norm"
+python -m project.shallow_models.model_trainer --dataset "karate" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_no_norm_degree_information_karate" --degree_information
+python -m project.shallow_models.model_trainer --dataset "cora" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_no_norm_degree_information_cora" --degree_information
+python -m project.shallow_models.model_trainer --dataset "citeseer" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_no_norm_degree_information_citeceer" --degree_information
+python -m project.shallow_models.model_trainer --dataset "pubmed" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_no_norm_degree_information_pubmed" --degree_information
+python -m project.shallow_models.model_trainer --dataset "computers" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_no_norm_degree_information_computers" --degree_information
+python -m project.shallow_models.model_trainer --dataset "photo" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_no_norm_degree_information_photo" --degree_information
+echo "With degree_information"
+python -m project.shallow_models.model_trainer --dataset "karate" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_norm_no_degree_information_karate" --use_norm
+python -m project.shallow_models.model_trainer --dataset "cora" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_norm_no_degree_information_cora" --use_norm
+python -m project.shallow_models.model_trainer --dataset "citeseer" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_norm_no_degree_information_citeceer" --use_norm
+python -m project.shallow_models.model_trainer --dataset "pubmed" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_norm_no_degree_information_pubmed" --use_norm
+python -m project.shallow_models.model_trainer --dataset "computers" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_norm_no_degree_information_computers" --use_norm
+python -m project.shallow_models.model_trainer --dataset "photo" --gpu_count 0 --cpu_count 1 --model_name "gcn" --identifier "gcn_norm_no_degree_information_photo" --use_norm
