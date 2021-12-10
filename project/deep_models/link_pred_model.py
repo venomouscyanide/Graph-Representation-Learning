1e91ef164ebcf2f04c2d8ca5eb3a8a0b3056fca8
Paul Louis <iampaullouis@gmail.com>
2021-12-09 19:26:14 -0500

    Add more debug

--- project/deep_models/link_pred_model.py
+++ project/deep_models/link_pred_model.py
@@ -44,7 +44,7 @@ class LinkPredModel(torch.nn.Module):
         node_id_index = torch.tensor([_ for _ in range(x.size(0))])
         node_id_index.to(device)
         for _ in range(10):
-            print(x.get_device(), node_id_index.get_device())
+            print(x.get_device(), node_id_index.get_device(), move_to_cuda)
         if self.id_version:
             x = self.conv1(x, edge_index, id=node_id_index)
         else:
