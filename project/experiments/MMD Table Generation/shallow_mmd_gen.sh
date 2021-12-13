ca07a0dc7f389d77d109a19586deb87691d028eb
Paul Louis <iampaullouis@gmail.com>
2021-12-13 04:11:03 -0500

    Distinguish between the outputs

--- project/experiments/MMD Table Generation/shallow_mmd_gen.sh
+++ project/experiments/MMD Table Generation/shallow_mmd_gen.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python -m project.statistics.mmd_table_generation.shallow_stats_table --base_folder shallow_models --output_folder shallow_table_exp --mmd_type "clustering"
-python -m project.statistics.mmd_table_generation.shallow_stats_table --base_folder shallow_models --output_folder shallow_table_exp --mmd_type "deg_distribution"
-python -m project.statistics.mmd_table_generation.shallow_stats_table --base_folder shallow_models --output_folder shallow_table_exp --mmd_type "betweeness_centrality"
-python -m project.statistics.mmd_table_generation.shallow_stats_table --base_folder shallow_models --output_folder shallow_table_exp --mmd_type "closeness_centrality"
+python -m project.statistics.mmd_table_generation.shallow_stats_table --base_folder shallow_models --output_folder shallow_table_clustering_exp --mmd_type "clustering"
+python -m project.statistics.mmd_table_generation.shallow_stats_table --base_folder shallow_models --output_folder shallow_table_deg_dist_exp --mmd_type "deg_distribution"
+python -m project.statistics.mmd_table_generation.shallow_stats_table --base_folder shallow_models --output_folder shallow_table_betweeness_exp --mmd_type "betweeness_centrality"
+python -m project.statistics.mmd_table_generation.shallow_stats_table --base_folder shallow_models --output_folder shallow_table_closeness_exp --mmd_type "closeness_centrality"
